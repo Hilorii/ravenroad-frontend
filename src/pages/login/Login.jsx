e2b4f551963fa9360a0151410aa9ff5bfd0ddaf1
Hilori <hilori.furan@wp.pl>
2024-07-25 21:58:43 +0200

    login cddd

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,6 +3,13 @@ import { useState } from 'react';
 import './login.css';
 import logo from '../../assets/RRlogo.png';
 
+// document.getElementById('togglePassword').addEventListener('click', function () {
+//     const passwordInput = document.getElementById('password');
+//     const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+//     passwordInput.setAttribute('type', type);
+//     this.textContent = type === 'password' ? 'Pokaż' : 'Ukryj';
+// });
+
 const LoginPage = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -14,6 +21,12 @@ const LoginPage = () => {
         console.log('Password:', password);
     };
 
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
         <div className="bg">
             <div className="login-container">
@@ -22,8 +35,8 @@ const LoginPage = () => {
                 <h2>Jesteś gotowy na podróż?</h2>
                 <form onSubmit={handleSubmit}>
                     <div className="form-group">
-                        <label htmlFor="username">Nazwa użytkownika:</label>
                         <input
+                            placeholder="Nazwa użytkownika"
                             type="text"
                             id="username"
                             name="username"
@@ -31,17 +44,24 @@ const LoginPage = () => {
                             onChange={(e) => setUsername(e.target.value)}
                         />
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Hasło:</label>
+                    <div className="form-group password">
                         <input
+                            placeholder="Hasło"
                             type="password"
                             id="password"
                             name="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                         />
+                        <button type='button' id='togglePassword'>Pokaż</button>
                     </div>
-                    <button type="submit">Login</button>
+                    <button className='logIn' type="submit">Zaloguj się</button>
+                    <div className='link1'>
+                        <a href=''>Zapomniałeś hasła?</a>
+                    </div>
+                    <div className='link2'>
+                        <a href=''>Zarejestruj się!</a>
+                    </div>
                 </form>
             </div>
         </div>
