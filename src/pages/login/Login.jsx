86d3bef3cf6b683e2a4f4224f027a2cf4b93fce0
Hilori <hilori.furan@wp.pl>
2024-07-25 18:26:05 +0200

    login cd

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
-
-import {Footer, Blog, Possibility, Features, WhatRR, Header} from "../../containers";
-import {CTA, Brand, Navbar} from '../../components'
-
+import { useState } from 'react';
+import './login.css';
+import logo from '../../assets/RRlogo.png';
 
 const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Tutaj można dodać logikę logowania
+        console.log('Username:', username);
+        console.log('Password:', password);
+    };
+
     return (
-        <div>
-            <h1>WITAM</h1>
+        <div className="bg">
+            <div className="login-container">
+                <img src={logo} alt="Logo" className="login-logo"/>
+                <h1>Witaj ponownie!</h1>
+                <h2>Jesteś gotowy na podróż?</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="username">Username:</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
         </div>
-    )};
+    );
+};
 
 export default LoginPage;
