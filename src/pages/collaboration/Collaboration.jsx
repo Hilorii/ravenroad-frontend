b1fa05fce2b99e09b7b7a292d012b67069ccb283
Hilori <hilori.furan@wp.pl>
2024-11-19 00:09:57 +0100

    został ten dziwny kontener

--- src/pages/collaboration/Collaboration.jsx
+++ src/pages/collaboration/Collaboration.jsx
@@ -1,33 +1,35 @@
-import { YStack, XStack} from 'tamagui';
-import Navbar from "../../components/navbar/Navbar"
-import NavbarTmp from '../../components/navbar/NavbarTmp'
-import './collaboration.css'
-import { Link } from 'react-router-dom';
-import { MAIL } from '../../components/icons'
+import { YStack, XStack } from 'tamagui';
+import NavbarTmp from '../../components/navbar/NavbarTmp';
+import './collaboration.css';
+import { MAIL } from '../../components/icons';
 import { collaborationMail } from '../../components/info';
+import { useTranslation } from 'react-i18next';
 
 export default function CollaborationPage() {
+    const { t } = useTranslation();
+
     return (
         <div className="App">
             <div className="gradient__bg">
-                <NavbarTmp/>
+                <NavbarTmp />
                 <YStack padding="$4" space>
-                    <h1 className="gradient__text">Współpracuj z nami!</h1>
+                    <h1 className="gradient__text">{t('collaboration.title')}</h1>
                     <YStack padding="$10" space alignItems="center">
-                        <p className="collab-info"><a>Chcesz, aby Twoja firma była widoczna w naszej aplikacji nawigacyjnej? To doskonała okazja, by dotrzeć do nowych klientów! Skontaktuj się z nami, wysyłając wiadomość na <strong>{collaborationMail}</strong>. W odpowiedzi otrzymasz wszystkie szczegóły dotyczące współpracy. Dołącz do grona firm, które już z nami współpracują i zyskaj przewagę na rynku!</a></p>
+                        <p className="collab-info">
+                            <a>{t('collaboration.description')} <strong>{collaborationMail}</strong>. {t('collaboration.details')}</a>
+                        </p>
                     </YStack>
                     <YStack padding="$1" space alignItems="center">
-                        <p className="collab-info-mail">Email: <a href={`mailto:${collaborationMail}`}>{collaborationMail}</a></p>
+                        <p className="collab-info-mail">{t('collaboration.emailLabel')}: <a href={`mailto:${collaborationMail}`}>{collaborationMail}</a></p>
                     </YStack>
                     <XStack space justifyContent="center" flexWrap="wrap">
                         <div className="collab-item">
                             <MAIL size={40} color="#ffffff"/>
-                            <p><a href={`mailto:${collaborationMail}`}>Email</a></p>
+                            <p><a href={`mailto:${collaborationMail}`}>{t('collaboration.email')}</a></p>
                         </div>
                     </XStack>
                 </YStack>
             </div>
         </div>
-
     );
 }
