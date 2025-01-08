38c41ae109ccc4d7446ad80ef4cf48f9af780386
Hilori <hilori.furan@wp.pl>
2025-01-08 18:01:40 +0100

    POCZĄTEK WIELKICH ZMIAN

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -2,52 +2,75 @@ import React from 'react';
 import rrLogo from '../../assets/RRlogo.png';
 import './footer.css';
 import { contactMail } from '../../components/info';
-import { useTranslation } from 'react-i18next'; // Import i18n
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
     const { t, i18n } = useTranslation();
-
-    // Ustalenie prefiksu URL na podstawie aktualnego języka
     const currentLanguage = i18n.language;
 
     return (
         <div className="rr__footer section__padding">
-            {/*<div className="rr__footer-heading">*/}
-            {/*    <h1 className="gradient__text">{t('footer.readyForFuture')}</h1>*/}
-            {/*</div>*/}
-
-            {/*<div className="rr__footer-btn">*/}
-            {/*    <p>{t('footer.requestAccess')}</p>*/}
-            {/*</div>*/}
-
             <div className="rr__footer-links">
+                {/* Logo i nazwa */}
                 <div className="rr__footer-links_logo">
                     <img src={rrLogo} alt="rr_logo" />
-                    <p>Raven Road sp z o.o., <br />{t('footer.allRightsReserved')}</p>
+                    <p>
+                        Raven Road sp z o.o., <br />
+                        {t('footer.allRightsReserved')}
+                    </p>
                 </div>
+
+                {/* Pierwsza kolumna linków */}
                 <div className="rr__footer-links_div">
                     <h4>{t('footer.links')}</h4>
-                    {/*<p>{t('footer.stories')}</p>*/}
-                    <p><a href="/FAQ">{t('footer.faq')}</a></p>
-                    <p><a href="/help">{t('footer.help')}</a></p>
-                    <p><a href={`/${currentLanguage}/termsofuse`}>{t('footer.termsOfUse')}</a></p>
-                    {/*<p><a href={`/${currentLanguage}/privacypolicy`}>{t('footer.privacyPolicy')}</a></p>*/}
+                    <p>
+                        <a href="/FAQ">{t('footer.faq')}</a>
+                    </p>
+                    <p>
+                        <a href="/help">{t('footer.help')}</a>
+                    </p>
+                    <p>
+                        <a href={`/${currentLanguage}/termsofuse`}>{t('footer.termsOfUse')}</a>
+                    </p>
                 </div>
+
+                {/* Druga kolumna linków */}
                 <div className="rr__footer-links_div">
                     <h4>{t('footer.company')}</h4>
-                    {/*<p><a href="/about">{t('footer.aboutUs')}</a></p>*/}
-                    {/*<p><a href="/pricing">{t('footer.ravenPro')}</a></p>*/}
-                    {/*<p><a href="/team">{t('footer.team')}</a></p>*/}
-                    <p><a href="/contact">{t('footer.contact')}</a></p>
+                    <p>
+                        <a href="/contact">{t('footer.contact')}</a>
+                    </p>
                 </div>
+
+                {/* Trzecia kolumna – ikony i teksty */}
                 <div className="rr__footer-links_div">
                     <h4>{t('footer.heading')}</h4>
-                    <p>Discord: {t('footer.comingSoon')}</p>
-                    <p><a href="https://www.facebook.com/profile.php?id=61564039045420">Facebook</a></p>
-                    <p>Email: {contactMail}</p>
+
+                    {/* Discord */}
+                    <div className="rr__footer-link-item">
+                        <span className="rr__footer-icon"></span>
+                        <p>Discord: {t('footer.comingSoon')}</p>
+                    </div>
+
+                    {/* Facebook */}
+                    <div className="rr__footer-link-item">
+                        <span className="rr__footer-icon"></span>
+                        <p>
+                            <a href="https://www.facebook.com/profile.php?id=61564039045420">
+                                Facebook
+                            </a>
+                        </p>
+                    </div>
+
+                    {/* Email */}
+                    <div className="rr__footer-link-item">
+                        <span className="rr__footer-icon"></span>
+                        <p>Email: {contactMail}</p>
+                    </div>
                 </div>
             </div>
 
+            {/* Stopka kopiuj/prawo */}
             <div className="rr__footer-copyright">
                 <p>@2024 Raven Road. {t('footer.allRightsReserved')}</p>
             </div>
