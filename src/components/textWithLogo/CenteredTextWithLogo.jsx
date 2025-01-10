a6fa90b71aa0e3ca267c7d4b81e4e5e757fad002
Hilori <hilori.furan@wp.pl>
2025-01-10 13:22:25 +0100

    poprakwi małe kosmetyczne

--- src/components/textWithLogo/CenteredTextWithLogo.jsx
+++ src/components/textWithLogo/CenteredTextWithLogo.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './centeredTextWithLogo.css';
 import rrLogo from '../../assets/RRlogo.png';
 
 const CenteredTextWithLogo = () => {
+    // Hook do debugowania, aby upewnić się, że komponent renderuje
+    useEffect(() => {
+        console.log('CenteredTextWithLogo rendered');
+    }, []);
+
     return (
         <div className="centered-container">
             {/* Logo w tle */}
@@ -11,19 +16,17 @@ const CenteredTextWithLogo = () => {
             </div>
 
             {/* Wcięty tekst */}
-            <div className="cutout-text">
-                RAVEN ROAD
-            </div>
-            <div className="text">
-                Nawigacja przyszłości
-            </div>
+            <div className="cutout-text">RAVEN ROAD</div>
+
+            {/* Tekst, który się pojawia */}
+            <div className="text-ctwl animated-text-ctwl">Nawigacja przyszłości</div>
 
             {/* Przyciski */}
             <div className="button-container">
-                <a href="https://example.com" className="gradient-button">
+                <a href="https://example.com" className="gradient-button animated-button">
                     Przycisk 1
                 </a>
-                <a href="https://example.com" className="white-button">
+                <a href="https://example.com" className="white-button animated-button">
                     Przycisk 2
                 </a>
             </div>
