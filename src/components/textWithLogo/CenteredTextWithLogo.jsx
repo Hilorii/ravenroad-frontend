c595ee77e7940627413d717e6dcee69e45e76df6
Hilori <hilori.furan@wp.pl>
2025-01-10 12:58:28 +0100

    poprakwi małe kosmetyczne

--- src/components/textWithLogo/CenteredTextWithLogo.jsx
+++ src/components/textWithLogo/CenteredTextWithLogo.jsx
@@ -15,7 +15,17 @@ const CenteredTextWithLogo = () => {
                 RAVEN ROAD
             </div>
             <div className="text">
-                RAVEN ROAD
+                Nawigacja przyszłości
+            </div>
+
+            {/* Przyciski */}
+            <div className="button-container">
+                <a href="https://example.com" className="gradient-button">
+                    Przycisk 1
+                </a>
+                <a href="https://example.com" className="white-button">
+                    Przycisk 2
+                </a>
             </div>
         </div>
     );
