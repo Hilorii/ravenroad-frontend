83b2fed94cbb3ce7f3ac4da6d3e966f162445bf4
Hilori <hilori.furan@wp.pl>
2025-01-29 21:47:22 +0100

    grupy

--- src/pages/groups/Groups.jsx
+++ src/pages/groups/Groups.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import { useUser } from '../../contexts/UserContext';
 import Navbar from "../../components/navbar/Navbar";
 import './Groups.css';
 import { Link } from 'react-router-dom';
 import AnimatedBackground from '../../assets/AnimatedBackground/AnimatedBackground';
 import Footer from '../../containers/footer/Footer';
-// Ikony przykładowo z react-icons (npm install react-icons)
-import { FaEdit, FaInfoCircle, FaSignOutAlt } from 'react-icons/fa';
+
+// Ikony z react-icons
+import { FaEdit, FaInfoCircle, FaSignOutAlt, FaPlus, FaCrown } from 'react-icons/fa';
 
 export default function Groups() {
+    const { user } = useUser(); // Pobieramy obiekt `user` z kontekstu
     const [userGroups, setUserGroups] = useState([]);
     const [proposedGroups, setProposedGroups] = useState([]);
     const [error, setError] = useState(null);
@@ -17,9 +20,10 @@ export default function Groups() {
         fetchProposedGroups();
     }, []);
 
+    // Funkcja pobierania "Twoich grup"
     const fetchUserGroups = async () => {
         try {
-            const token = localStorage.getItem('token'); // lub skąd pobierasz token
+            const token = localStorage.getItem('token');
             const response = await fetch('http://localhost:3000/groups', {
                 headers: {
                     Authorization: `Bearer ${token}`,
@@ -37,9 +41,10 @@ export default function Groups() {
         }
     };
 
+    // Funkcja pobierania "Proponowanych grup"
     const fetchProposedGroups = async () => {
         try {
-            const token = localStorage.getItem('token'); // lub skąd pobierasz token
+            const token = localStorage.getItem('token');
             const response = await fetch('http://localhost:3000/searchGroups', {
                 headers: {
                     Authorization: `Bearer ${token}`,
@@ -47,7 +52,6 @@ export default function Groups() {
             });
 
             if (!response.ok) {
-                // Jeśli np. zwracasz 404 przy braku wyników, możesz obsłużyć to inaczej.
                 throw new Error('Błąd podczas pobierania proponowanych grup.');
             }
 
@@ -58,7 +62,7 @@ export default function Groups() {
         }
     };
 
-    // Przykładowe „puste” metody do obsługi ikon:
+    // Przykładowe metody do obsługi akcji
     const handleEditGroup = (groupId) => {
         console.log("Edycja grupy ID:", groupId);
     };
@@ -71,11 +75,15 @@ export default function Groups() {
         console.log("Opuszczenie grupy ID:", groupId);
     };
 
+    const handleJoinGroup = (groupId) => {
+        console.log("Dołączanie do grupy ID:", groupId);
+    };
+
     return (
         <div>
-            <AnimatedBackground/>
-            <Navbar/>
-            {/* Dodaj napis "Grupy" */}
+            <AnimatedBackground />
+            <Navbar />
+            {/* Napis "Grupy" */}
             <h1 className="groups-title">Grupy</h1>
             <div className="groups-wrapper">
                 {/* PIERWSZY PROSTOKĄT: TWOJE GRUPY */}
@@ -85,6 +93,10 @@ export default function Groups() {
                         {error && <p className="error-message">{error}</p>}
                         {userGroups.map((group) => (
                             <div key={group.id} className="group-item">
+                                {/* Złota koronka dla właściciela grupy */}
+                                {user && group.created_by === user.id && (
+                                    <FaCrown className="group-crown" />
+                                )}
                                 <img
                                     src={`http://localhost:5000/uploads/${group.image}`}
                                     alt={group.name}
@@ -92,11 +104,14 @@ export default function Groups() {
                                 />
                                 <span className="group-name">{group.name}</span>
                                 <div className="group-actions">
-                                    <FaEdit
-                                        className="group-icon"
-                                        title="Edytuj grupę"
-                                        onClick={() => handleEditGroup(group.id)}
-                                    />
+                                    {/* Ikona edycji widoczna tylko dla właściciela */}
+                                    {user && group.created_by === user.id && (
+                                        <FaEdit
+                                            className="group-icon"
+                                            title="Edytuj grupę"
+                                            onClick={() => handleEditGroup(group.id)}
+                                        />
+                                    )}
                                     <FaInfoCircle
                                         className="group-icon"
                                         title="Detale grupy"
@@ -132,10 +147,11 @@ export default function Groups() {
                                         title="Detale grupy"
                                         onClick={() => handleViewDetails(group.id)}
                                     />
-                                    <FaSignOutAlt
+                                    {/* Zmieniona ikona na dołączanie */}
+                                    <FaPlus
                                         className="group-icon"
-                                        title="Opuść grupę"
-                                        onClick={() => handleLeaveGroup(group.id)}
+                                        title="Dołącz do grupy"
+                                        onClick={() => handleJoinGroup(group.id)}
                                     />
                                 </div>
                             </div>
@@ -143,7 +159,7 @@ export default function Groups() {
                     </div>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
